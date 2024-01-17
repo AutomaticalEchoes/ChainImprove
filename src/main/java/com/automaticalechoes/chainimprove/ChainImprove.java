aa8fc7af3df1d41db67d9c68835bcd3d61424c1b
1121665965 <1121665965@qq.com>
2024-01-17 21:07:18 +0800

    1.19

---
+++ src/main/java/com/automaticalechoes/chainimprove/ChainImprove.java
@@ -0,0 +1,51 @@
+package com.automaticalechoes.chainimprove;
+
+import com.automaticalechoes.chainimprove.api.ChainBlockItem;
+import com.automaticalechoes.chainimprove.common.ChainKnotEntity;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Optional;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ChainImprove.MODID)
+public class ChainImprove
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "chainimprove";
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
+    public static final RegistryObject<EntityType<ChainKnotEntity>> CHAIN_KNOT_ENTITY = ENTITY_TYPES.register("chain_knot",
+            () -> EntityType.Builder.of(ChainKnotEntity::Create, MobCategory.MISC)
+                    .sized(0.375F, 0.5F)
+                    .clientTrackingRange(10)
+                    .updateInterval(Integer.MAX_VALUE).build("chain_knot"));
+    public ChainImprove()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::OnRegistry);
+        ENTITY_TYPES.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public void OnRegistry(RegisterEvent event){
+        if(event.getRegistryKey().equals(ForgeRegistries.Keys.ITEMS)){
+            ResourceLocation key = ForgeRegistries.ITEMS.getKey(Items.CHAIN);
+//            ChainBlockItem.CHAIN_OVERRIDE.builtInRegistryHolder().bindValue(ChainBlockItem.CHAIN_OVERRIDE);
+            ForgeRegistries.ITEMS.register(key, ChainBlockItem.CHAIN_OVERRIDE);
+        }
+    }
+
+
+}
